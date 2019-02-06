b4358d0007960ac34ad18ce36e91724ce0a11a23
Frank Murphy <fmurphy@instructure.com>
2019-02-05 21:24:49 -0500

    Add some basic comments, fix spacing

--- petrify/solid.py
+++ petrify/solid.py
@@ -6,7 +6,6 @@ from csg import core, geom
 
 tau = math.pi * 2
 
-
 def perpendicular(axis):
     "Return a vector that is perpendicular to the given axis."
     if axis.x == 0 and axis.y == 0:
@@ -20,6 +19,11 @@ def vertex(vector):
     return geom.Vertex(geom.Vector(vector.x, vector.y, vector.z))
 
 class Projection:
+    """
+    Used for building solids from slices of two-dimensional geometry along a
+    given z-axis.
+
+    """
     def __init__(self, origin, bx, by, bz):
         self.origin = origin
         self.bx = bx
@@ -35,6 +39,7 @@ class Projection:
         return Vector3(v.x, v.y, v.z)
 
 class Slice:
+    """ A slice of two-dimensional geometry associated with a z-level. """
     def __init__(self, points, dz):
         self.points = points
         self.dz = dz
@@ -43,6 +48,22 @@ class Slice:
         return [projection.convert(p, self.dz) for p in self.points]
 
 class Extrusion:
+    """
+    A three-dimensional object built from varying height slices of 2d polygons.
+
+    The slices must currently all have the same number of vertices. Quads are
+    generated to connect each layer, and the bottom and top layers then complete
+    the shape.
+
+    Parameters
+    ----------
+    projection :
+        A :class:`Projection` defining the basis for the projected shape.
+    slices :
+        A list of :class:`Slice` objects defining each layer of the final shape.
+
+    """
+
     def __init__(self, projection, slices):
         self.projection = projection
         assert(len(set(len(sl.points) for sl in slices)) == 1)
@@ -52,6 +73,7 @@ class Extrusion:
         self.csg = core.CSG.fromPolygons(ps)
 
     def polygons(self):
+        """ Returns all polygons from this shape. """
         bottom = self.slices[0].project(self.projection)
         top = self.slices[-1].project(self.projection)
 
@@ -67,6 +89,7 @@ class Extrusion:
         ]
 
     def ring(self, a, b):
+        """ Builds a ring from two slices. """
         bottom = a.project(self.projection)
         top = b.project(self.projection)
         lines = list(zip(bottom, top))
@@ -74,6 +97,18 @@ class Extrusion:
                  for la, lb in zip(lines, lines[1:] + [lines[0]])]
 
 class Node:
+    """
+    Convenience class for performing CSG operations on geometry.
+
+    All instances of this class can be added and subtracted via the built-in
+    `__add__` and `__sub__` methods:
+
+    >>> a = Box(Vector3(0, 0, 0), Vector3(1, 1, 1))
+    >>> b = Box(Vector3(0, 0, 0.5), Vector3(1, 1, 1))
+    >>> a + b # union
+    >>> a - b # subtraction
+
+    """
     def __init__(self, csg):
         self.csg = csg
 
@@ -90,16 +125,22 @@ class Node:
         return n
 
     def scale(self, scale):
+        """ Scale this geometry by the provided `scale` vector. """
         def scaled(v):
             return Vector3(v.x * scale.x, v.y * scale.y, v.z * scale.z)
         return self.transform(scaled)
 
     def translate(self, delta):
+        """ Translate this geometry by the provided `translate` vector. """
         def scaled(v):
             return Vector3(v.x + delta.x, v.y + delta.y, v.z + delta.z)
         return self.transform(scaled)
 
+    def rotate(self, axis, theta):
+        """ Rotate this geometry around the given `axis` vector by `theta` radians. """
+
     def transform(self, f):
+        """ Map the specified function `f` across all vertices. """
         polygons = self.csg.toPolygons()
 
         def t(vertex):
@@ -113,10 +154,12 @@ class Node:
         return Node(core.CSG.fromPolygons(scaled))
 
     def to_stl(self, path):
+        """ Save this shape to an STL-formatted file. """
         polygons = self.csg.toPolygons()
         save_polys_to_stl_file(polygons, path)
 
 class Union(Node):
+    """ Defines a union of a list of `parts` """
     def __init__(self, parts):
         whole = parts[0].csg
         for part in parts[1:]:
@@ -125,6 +168,12 @@ class Union(Node):
         self.csg = whole
 
 class Box(Extrusion, Node):
+    """
+    A simple three-dimensional box constructed from a given `origin` and `size`
+    vector.
+
+    """
+
     def __init__(self, origin, size):
         self.origin = origin
         self.extent = extent = origin + size
@@ -150,6 +199,15 @@ class Box(Extrusion, Node):
         return self.extent - self.origin
 
 class Cylinder(Extrusion, Node):
+    """
+    A three-dimensional cylinder extruded along the given `axis`. The magnitude
+    of the axis is the height of the cylinder.
+
+    The actual cylinder is constructed from many `segments` of quads to simulate
+    a circular shape.
+
+    """
+
     def __init__(self, origin, axis, radius, segments=10):
         self.origin = origin
         self.axis = axis
