e8b820bd3c9720c7ac0371046b882dd92897be36
Frank Murphy <fpmurphy@mtu.edu>
2019-03-30 17:45:15 -0400

    Remove f-strings for compatibility

--- petrify/units.py
+++ petrify/units.py
@@ -7,15 +7,15 @@ def parse_unit(v):
     return getattr(u, v) if isinstance(v, str) else v
 
 def assert_lengthy(u):
-    assert hasattr(u, 'magnitude'), f"object does not have unit tag: {u}"
-    assert u.check('[length]'), f"does not have length units: {u}"
+    assert hasattr(u, 'magnitude'), "object does not have unit tag: {0!r}".format(u)
+    assert u.check('[length]'), "does not have length units: {0!r}".format(u)
     return u
 
 def conversion(scale):
     scale = parse_unit(scale)
     unit_or_quantity = isinstance(scale, u.Unit) or isinstance(scale, u.Quantity)
-    assert unit_or_quantity, f"object not unit or scale conversion: {scale}"
+    assert unit_or_quantity, "object not unit or scale conversion: {0!r}".format(scale)
     if isinstance(scale, u.Unit):
         scale = (1 * scale) / u.file
-    assert scale.check('[length] / [file]'), f"does not have scaling units ([length] / [file]): {scale.units}"
+    assert scale.check('[length] / [file]'), "does not have scaling units ([length] / [file]): {0!r}".format({scale.units})
     return scale
