459b8a9f79caf644a495d66fbe3e11711c3a7dd3
Frank Murphy <fpmurphy@mtu.edu>
2019-03-30 17:45:29 -0400

    Switch to new unit specification I/O

--- examples/svg.py
+++ examples/svg.py
@@ -1,10 +1,10 @@
 from petrify.solid import Projection
-from petrify.formats import svg
+from petrify.formats import svg, SVG, STL
 
 def shape():
-    paths = svg.SVG.read('tests/fixtures/example.svg', 'mm')
+    paths = SVG.read('tests/fixtures/example.svg', 'mm')
     box = paths['rect']
     return svg.PathExtrusion(box, 100.0, Projection.unit)
 
 if __name__ == '__main__':
-    shape().to_stl('output/svg.stl')
+    STL('output/svg.stl', 'mm').write(shape().as_unit('mm'))
