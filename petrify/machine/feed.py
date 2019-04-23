7a6f9c6e44a797ef1dd62210e4c8e654416c6b6d
Frank Murphy <fpmurphy@mtu.edu>
2019-04-23 18:35:29 -0400

    Fix frange inclusivity

--- petrify/machine/feed.py
+++ petrify/machine/feed.py
@@ -3,11 +3,13 @@ import math
 from ..plane import Line, LineSegment, Point, Vector
 from ..decompose import grouper
 
-def frange(a, b, step):
-    count = math.floor((b - a) / step)
+def frange(a, b, step, inclusive=False):
+    count = math.floor((b - a) / step) if a != b else 1
     for ix in range(0, count):
         v = a + (ix * step)
         if v != b: yield v
+
+    if inclusive: yield b
 
 class PlanarToolpath:
     """
