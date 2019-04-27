9e7e8c39cdc50951465e9a8f4da01649a445d7f3
Frank Murphy <fpmurphy@mtu.edu>
2019-04-27 19:01:56 -0400

    Refactor out commands

--- petrify/machine/__init__.py
+++ petrify/machine/__init__.py
@@ -1,5 +1,6 @@
 from .part import Part, Tab
 from .pocket import Pocket
 from .feed import LinearStepFeed
-from .motion import Batch, ToolPause, ToolChange, Pause
+from .motion import Batch
+from .commands import ToolPause, ToolChange, Pause
 from .tool import StraightTip, Machine, Speed
--- petrify/machine/motion.py
+++ petrify/machine/motion.py
@@ -59,36 +59,6 @@ class Motion:
         parts.append('F{0}'.format(self.f))
         return ' '.join(parts)
 
-class GCode:
-    def __init__(self, line):
-        self.line = line
-
-    def __add__(self, v):
-        if not isinstance(v, Vector):
-            return NotImplemented
-        return self
-
-    def gcode(self):
-        return self.line
-
-class ToolChange:
-    def __init__(self, tool):
-        self.tool = tool
-
-    def commands(self):
-        return [(self, GCode('M6 T{0}'.format(self.tool.number)))]
-
-class Pause:
-    def __init__(self, message):
-        self.message = message
-
-    def commands(self):
-        return [(self, GCode('M0 {0}'.format(self.message)))]
-
-class ToolPause:
-    def __init__(self, tool):
-        super().__init__('Tool: {0}'.format(tool.name))
-
 class Cut:
     def props(self, **props):
         return AnnotatedPhase(self, props)
