cdad4232b56e6d4ed259e439a5ab9f257afb38d7
Frank Murphy <fpmurphy@mtu.edu>
2019-05-09 23:47:23 -0400

    Wow. yay namespace packages.

--- examples/svgx.py
+++ examples/svgx.py
@@ -1,10 +1,11 @@
 from petrify.solid import Projection
-from petrify.formats import svg, SVG, STL
+from petrify.formats import SVG, STL
+from petrify.formats.svg import PathExtrusion
 
 def shape():
     paths = SVG.read('tests/fixtures/example.svg', 'mm')
     box = paths['rect']
-    return svg.PathExtrusion(box, 100.0, Projection.unit)
+    return PathExtrusion(box, 100.0, Projection.unit)
 
 if __name__ == '__main__':
     STL('output/svg.stl', 'mm').write(shape().as_unit('mm'))
