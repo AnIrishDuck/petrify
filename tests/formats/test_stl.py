301bb7adddfa479db4c979e02d08409a31595e05
Frank Murphy <fpmurphy@mtu.edu>
2019-05-16 04:21:38 -0400

    Fix tests

--- tests/formats/test_stl.py
+++ tests/formats/test_stl.py
@@ -1,6 +1,5 @@
 import doctest, unittest
 from petrify.formats import stl
-from petrify.solid import Projection
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(stl))
