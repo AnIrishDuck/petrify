e2424d709a8a6418f482e58b422b00b8ff90d5df
Frank Murphy <fpmurphy@mtu.edu>
2019-05-16 04:25:01 -0400

    Fix build

--- examples/svgx.py
+++ examples/svgx.py
@@ -1,11 +1,11 @@
-from petrify.solid import Projection
+from petrify.solid import Basis, Vector
 from petrify.formats import SVG, STL
 from petrify.formats.svg import PathExtrusion
 
 def shape():
     paths = SVG.read('tests/fixtures/example.svg', 'mm')
     box = paths['rect']
-    return PathExtrusion(box, 100.0, Projection.unit)
+    return PathExtrusion(Basis.unit, box, Vector.basis.z * 10)
 
 if __name__ == '__main__':
     STL('output/svg.stl', 'mm').write(shape().as_unit('mm'))
