cee75cb4852f7e251c3f66dc77f03584ce0c0a5b
Frank Murphy <fmurphy@instructure.com>
2019-06-30 22:01:08 -0400

    Fix examples

--- examples/svgx.py
+++ examples/svgx.py
@@ -1,11 +1,10 @@
-from petrify.solid import Basis, Vector
+from petrify.solid import Basis, Vector, PolygonExtrusion, PlanarPolygon
 from petrify.formats import SVG, STL
-from petrify.formats.svg import PathExtrusion
 
 def shape():
     paths = SVG.read('tests/fixtures/example.svg', 'mm')
-    box = paths['rect']
-    return PathExtrusion(Basis.unit, box, Vector.basis.z * 10)
+    box = paths['rect'].polygon()
+    return PolygonExtrusion(PlanarPolygon(Basis.xy, box), Vector.basis.z * 10)
 
 if __name__ == '__main__':
     STL('output/svg.stl', 'mm').write(shape().as_unit('mm'))
