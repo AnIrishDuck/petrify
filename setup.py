13103306fd9c40f057fff751121b1ecac61e4b95
Frank Murphy <fmurphy@instructure.com>
2019-10-01 19:38:17 -0400

    0.7.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='petrify',
-    version='0.4.0',
+    version='0.7.0',
     author='Frank Murphy, et al',
     author_email='fpmurphy@mtu.edu',
     packages=['petrify', 'petrify.engines', 'petrify.formats', 'petrify.machine'],
