5e8f830c524e23c16274db4b625db3bd8347e1fa
Frank Murphy <fmurphy@instructure.com>
2019-10-03 00:01:33 -0400

    Add generics to top-level export

--- petrify/__init__.py
+++ petrify/__init__.py
@@ -1,2 +1,6 @@
 from .units import u
 from .geometry import tau
+
+from .generic import Point, Vector, Polygon
+from .plane import Point2, Vector2, Polygon2
+from .space import Point3, Vector3, Polygon3
