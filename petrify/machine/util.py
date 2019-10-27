f3e6e6e81890beeb881efb19dd87886e8e12f5b2
Frank Murphy <fmurphy@instructure.com>
2019-10-27 18:25:26 -0400

    Begin work on clearance calculations

--- petrify/machine/util.py
+++ petrify/machine/util.py
@@ -9,6 +9,13 @@ class CutLine:
         self.start = start
         self.end = end
 
+    def interpolate(self, v):
+        (sv, sp, sr), (ev, ep, er) = self.start, self.end
+        progress = None if ev == sv else (v - sv) / (ev - sv)
+        center = sp if progress is None else (progress * (ep - sp)) + sp
+        r = sr if progress is None else (progress * (er - sr)) + sr
+        return (center, r)
+
     @classmethod
     def from_lines(cls, a, b):
         def _close(p, r, l):
@@ -21,3 +28,27 @@ class CutLine:
         midpoints = [(x, (p, r, x)) for p, r, x in midpoints if _close(p, r, a)]
         ordered = [(x, p, r) for x, (p, r, _) in sorted(endpoints + midpoints, key=lambda v: (v[0], v[1][1]))]
         return None if len(ordered) < 2 else CutLine(ordered[0], ordered[-1])
+
+def clearance(segment, others):
+    cuts = [CutLine.from_lines(segment, other) for other in others]
+    cuts = [c for c in cuts if c is not None]
+
+    ordered = sorted(cuts, key=lambda c: c.start[0])
+    active = []
+    path = []
+    prior = None
+
+    ix = 0
+
+    points = sorted(set([p[0] for l in ordered for p in (l.start, l.end)]))
+    for position in sorted(points):
+        while ix < len(ordered) and ordered[ix].start[0] <= position:
+            active.append(ordered[ix])
+            ix += 1
+        active = [line for line in active if position <= line.end[0]]
+
+        places = [cut.interpolate(position) for cut in active]
+        places = sorted(places, key=lambda pair: pair[1])
+        path.append(places[0])
+
+    return path
--- tests/machine/test_util.py
+++ tests/machine/test_util.py
@@ -1,8 +1,8 @@
 import unittest
 
-from petrify import Point
+from petrify import Point, Polygon
 from petrify.plane import LineSegment2
-from petrify.machine.util import CutLine
+from petrify.machine.util import clearance, CutLine
 
 class TestCutLine(unittest.TestCase):
     def test_parallel_lines(self):
@@ -16,3 +16,27 @@ class TestCutLine(unittest.TestCase):
         reversed = CutLine.from_lines(b, a)
         self.assertEqual(reversed.start, (0.5, Point(0, 0.5), 0.5))
         self.assertEqual(reversed.end, (0.75, Point(1, 0.5), 0.5))
+
+class TestClearance(unittest.TestCase):
+    def test_indent(self):
+        segments = Polygon([
+            Point(0, 0),
+            Point(0, 6),
+            Point(2, 6),
+            Point(2, 4),
+            Point(1, 4),
+            Point(1, 2),
+            Point(2, 2),
+            Point(2, 0)
+        ]).segments()
+
+        path = clearance(segments[0], segments[1:])
+        expected = [
+            (Point(0.0, 0.0), 0.0),
+            (Point(1.0, 1.0), 1.0),
+            (Point(0.5, 2.0), 0.5),
+            (Point(0.5, 4.0), 0.5),
+            (Point(1.0, 5.0), 1.0),
+            (Point(0.0, 6.0), 0.0)
+        ]
+        self.assertEqual([(c.snap(0.25), round(r, 1)) for c, r in path], expected)
