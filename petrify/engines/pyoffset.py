f74bfb61dd2c91e519b5d2287ffafaabe09d1fb3
Frank Murphy <fmurphy@instructure.com>
2019-12-11 22:02:28 -0500

    Refactor into rays bundles

--- petrify/engines/pyoffset.py
+++ petrify/engines/pyoffset.py
@@ -25,7 +25,7 @@ def offset_inward_ray(polygon, ix):
 
     a, b = segments[0], segments[1]
     ai, bi = polygon.inwards(a), polygon.inwards(b)
-    return (shifted.points[1], magnitude(a.v, ai, ai + bi))
+    return Ray(shifted.points[1], magnitude(a.v, ai, ai + bi))
 
 def find_inward_ray(polygon, edge):
     mid = (edge.p1 + edge.p2) / 2
@@ -55,27 +55,30 @@ def find_collision(line, normal, vertex, inwards):
     solution = solve_matrix(matrix)
     return (line.p + line.v * solution[0] + normal * solution[1], solution[1])
 
-def find_collisions(v, segments, inwards, in_rays):
+def find_collisions(ray, segments, inwards):
     initial = (
-        ((other, v), find_collision(other, inwards[other], v, in_rays[v]))
-        for other in segments if other.p1 != v and other.p2 != v
+        ((other, ray.p), find_collision(other, inwards[other], ray.p, ray.v))
+        for other in segments if other.p1 != ray.p and other.p2 != ray.p
     )
     return ((pair, p, o) for pair, (p, o) in initial if o > 0)
 
 def inner_properties(polygon):
     segments = polygon.segments()
 
-    in_rays = dict(offset_inward_ray(polygon, ix) for ix in range(len(polygon)))
     inwards = {s: find_inward_ray(polygon, s) for s in segments}
 
-    return (segments, in_rays, inwards)
+    return (segments, inwards)
 
-def find_first_split_event(polygon):
-    segments, in_rays, inwards = inner_properties(polygon)
+def to_rays(polygon):
+    return [offset_inward_ray(polygon, ix) for ix in range(len(polygon))]
+
+def find_first_split_event(rays):
+    polygon = Polygon([ray.p for ray in rays])
+    segments, inwards = inner_properties(polygon)
 
     solutions = [
-        min(find_collisions(v, segments, inwards, in_rays), key=lambda t: t[2])
-        for v in polygon.points
+        min(find_collisions(ray, segments, inwards), key=lambda t: t[2])
+        for ray in rays
     ]
 
     (segment, cut), point, offset = min(solutions, key=lambda t: t[2])
@@ -83,12 +86,13 @@ def find_first_split_event(polygon):
     cut_i = polygon.index_of(cut)
     line_i = polygon.index_of(segment.p1)
 
-    a = [p + in_rays[p] * offset for p in (*polygon.points[0:line_i + 1], *polygon.points[cut_i:])]
-    b = [p + in_rays[p] * offset for p in polygon.points[(line_i + 1):(cut_i + 1)]]
+    a = [ray.p + ray.v * offset for ray in (*rays[0:line_i + 1], *rays[cut_i:])]
+    b = [ray.p + ray.v * offset for ray in rays[(line_i + 1):(cut_i + 1)]]
     return (offset, ComplexPolygon([Polygon(p) for p in (a, b) if p]))
 
 def nonlocal_offset(complex, amount):
-    offsets = [(polygon, find_first_split_event(polygon)) for polygon in complex.exterior]
+    offsets = [(polygon, find_first_split_event(to_rays(polygon)))
+               for polygon in complex.exterior]
     polygons = [
         p
         for original, (event_offset, split) in offsets
