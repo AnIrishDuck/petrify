ffc49e5cc8428d0722b28548d6c87a78f688dc4f
Frank Murphy <fmurphy@instructure.com>
2020-01-23 23:36:01 -0500

    Begin space refactor

--- petrify/space/__init__.py
+++ petrify/space/__init__.py
@@ -44,11 +44,23 @@ from .. import decompose, generic, plane, visualize
 from ..plane import Point2, Polygon2, Vector2
 from ..geometry import AbstractPolygon, Geometry, tau, valid_scalar
 
-class Spatial:
-    @property
-    def embedding(self):
-        from petrify import space
-        return space
+from .util import (
+    Spatial,
+    _connect_point3_line3,
+    _connect_point3_sphere,
+    _connect_point3_plane,
+    _connect_line3_line3,
+    _connect_line3_plane,
+    _connect_sphere_line3,
+    _connect_sphere_sphere,
+    _connect_sphere_plane,
+    _connect_plane_plane,
+    _intersect_point3_sphere,
+    _intersect_line3_sphere,
+    _intersect_line3_plane,
+    _intersect_plane_plane,
+    _pmap
+)
 
 def operate(op, self, other):
     if valid_scalar(other):
@@ -1866,187 +1878,3 @@ class Face(PlanarPolygon):
         else:
             simple = decompose.trapezoidal(self.polygon.polygons)
         return [Face(self.basis, self.direction, p).project()[0] for p in simple]
-
-
-# 3D Geometry
-# -------------------------------------------------------------------------
-
-def _connect_point3_line3(P, L):
-    d = L.v.magnitude_squared()
-    assert d != 0
-    u = ((P.x - L.p.x) * L.v.x + \
-         (P.y - L.p.y) * L.v.y + \
-         (P.z - L.p.z) * L.v.z) / d
-    if not L._u_in(u):
-        u = max(min(u, 1.0), 0.0)
-    return LineSegment3(P, Point3(L.p.x + u * L.v.x,
-                                  L.p.y + u * L.v.y,
-                                  L.p.z + u * L.v.z))
-
-def _connect_point3_sphere(P, S):
-    v = P - S.c
-    v.normalize()
-    v *= S.r
-    return LineSegment3(P, Point3(S.c.x + v.x, S.c.y + v.y, S.c.z + v.z))
-
-def _connect_point3_plane(p, plane):
-    n = plane.n.normalized()
-    d = p.dot(plane.n) - plane.k
-    return LineSegment3(p, Point3(p.x - n.x * d, p.y - n.y * d, p.z - n.z * d))
-
-def _connect_line3_line3(A, B):
-    assert A.v and B.v
-    p13 = A.p - B.p
-    d1343 = p13.dot(B.v)
-    d4321 = B.v.dot(A.v)
-    d1321 = p13.dot(A.v)
-    d4343 = B.v.magnitude_squared()
-    denom = A.v.magnitude_squared() * d4343 - d4321 ** 2
-    if denom == 0:
-        # Parallel, connect an endpoint with a line
-        if isinstance(B, Ray3) or isinstance(B, LineSegment3):
-            return _connect_point3_line3(B.p, A)._swap()
-        # No endpoint (or endpoint is on A), possibly choose arbitrary
-        # point on line.
-        return _connect_point3_line3(A.p, B)
-
-    ua = (d1343 * d4321 - d1321 * d4343) / denom
-    if not A._u_in(ua):
-        ua = max(min(ua, 1.0), 0.0)
-    ub = (d1343 + d4321 * ua) / d4343
-    if not B._u_in(ub):
-        ub = max(min(ub, 1.0), 0.0)
-    return LineSegment3(Point3(A.p.x + ua * A.v.x,
-                               A.p.y + ua * A.v.y,
-                               A.p.z + ua * A.v.z),
-                        Point3(B.p.x + ub * B.v.x,
-                               B.p.y + ub * B.v.y,
-                               B.p.z + ub * B.v.z))
-
-def _connect_line3_plane(L, P):
-    d = P.n.dot(L.v)
-    if not d:
-        # Parallel, choose an endpoint
-        return _connect_point3_plane(L.p, P)
-    u = (P.k - P.n.dot(L.p)) / d
-    if not L._u_in(u):
-        # intersects out of range, choose nearest endpoint
-        u = max(min(u, 1.0), 0.0)
-        return _connect_point3_plane(Point3(L.p.x + u * L.v.x,
-                                            L.p.y + u * L.v.y,
-                                            L.p.z + u * L.v.z), P)
-    # Intersection
-    return None
-
-def _connect_sphere_line3(S, L):
-    d = L.v.magnitude_squared()
-    assert d != 0
-    u = ((S.c.x - L.p.x) * L.v.x + \
-         (S.c.y - L.p.y) * L.v.y + \
-         (S.c.z - L.p.z) * L.v.z) / d
-    if not L._u_in(u):
-        u = max(min(u, 1.0), 0.0)
-    point = Point3(L.p.x + u * L.v.x, L.p.y + u * L.v.y, L.p.z + u * L.v.z)
-    v = (point - S.c)
-    v.normalize()
-    v *= S.r
-    return LineSegment3(Point3(S.c.x + v.x, S.c.y + v.y, S.c.z + v.z),
-                        point)
-
-def _connect_sphere_sphere(A, B):
-    v = B.c - A.c
-    d = v.magnitude()
-    if A.r >= B.r and d < A.r:
-        #centre B inside A
-        s1,s2 = +1, +1
-    elif B.r > A.r and d < B.r:
-        #centre A inside B
-        s1,s2 = -1, -1
-    elif d >= A.r and d >= B.r:
-        s1,s2 = +1, -1
-
-    v.normalize()
-    return LineSegment3(Point3(A.c.x + s1* v.x * A.r,
-                               A.c.y + s1* v.y * A.r,
-                               A.c.z + s1* v.z * A.r),
-                        Point3(B.c.x + s2* v.x * B.r,
-                               B.c.y + s2* v.y * B.r,
-                               B.c.z + s2* v.z * B.r))
-
-def _connect_sphere_plane(S, P):
-    c = _connect_point3_plane(S.c, P)
-    if not c:
-        return None
-    p2 = c.p2
-    v = p2 - S.c
-    v.normalize()
-    v *= S.r
-    return LineSegment3(Point3(S.c.x + v.x, S.c.y + v.y, S.c.z + v.z),
-                        p2)
-
-def _connect_plane_plane(A, B):
-    if A.n.cross(B.n):
-        # Planes intersect
-        return None
-    else:
-        # Planes are parallel, connect to arbitrary point
-        return _connect_point3_plane(A._get_point(), B)
-
-def _intersect_point3_sphere(P, S):
-    return abs(P - S.c) <= S.r
-
-def _intersect_line3_sphere(L, S):
-    a = L.v.magnitude_squared()
-    b = 2 * (L.v.x * (L.p.x - S.c.x) + \
-             L.v.y * (L.p.y - S.c.y) + \
-             L.v.z * (L.p.z - S.c.z))
-    c = S.c.magnitude_squared() + \
-        L.p.magnitude_squared() - \
-        2 * S.c.dot(L.p) - \
-        S.r ** 2
-    det = b ** 2 - 4 * a * c
-    if det < 0:
-        return None
-    sq = math.sqrt(det)
-    u1 = (-b + sq) / (2 * a)
-    u2 = (-b - sq) / (2 * a)
-    if not L._u_in(u1):
-        u1 = max(min(u1, 1.0), 0.0)
-    if not L._u_in(u2):
-        u2 = max(min(u2, 1.0), 0.0)
-    return LineSegment3(Point3(L.p.x + u1 * L.v.x,
-                               L.p.y + u1 * L.v.y,
-                               L.p.z + u1 * L.v.z),
-                        Point3(L.p.x + u2 * L.v.x,
-                               L.p.y + u2 * L.v.y,
-                               L.p.z + u2 * L.v.z))
-
-def _intersect_line3_plane(L, P):
-    d = P.n.dot(L.v)
-    if not d:
-        # Parallel
-        return None
-    u = (P.k - P.n.dot(L.p)) / d
-    if not L._u_in(u):
-        return None
-    return Point3(L.p.x + u * L.v.x,
-                  L.p.y + u * L.v.y,
-                  L.p.z + u * L.v.z)
-
-def _intersect_plane_plane(A, B):
-    n1_m = A.n.magnitude_squared()
-    n2_m = B.n.magnitude_squared()
-    n1d2 = A.n.dot(B.n)
-    det = n1_m * n2_m - n1d2 ** 2
-    if det == 0:
-        # Parallel
-        return None
-    c1 = (A.k * n2_m - B.k * n1d2) / det
-    c2 = (B.k * n1_m - A.k * n1d2) / det
-    return Line3(Point3(c1 * A.n.x + c2 * B.n.x,
-                        c1 * A.n.y + c2 * B.n.y,
-                        c1 * A.n.z + c2 * B.n.z),
-                 A.n.cross(B.n))
-
-def _pmap(Klass, f, it):
-    return Klass(f(v.x for v in it), f(v.y for v in it), f(v.z for v in it))
